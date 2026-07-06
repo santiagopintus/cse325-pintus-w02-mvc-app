316cd02f8e74d2054dd6ebb76c1b1fd790254bd4
Santiago Pintus <santiagopintus@gmail.com>
2026-07-06 16:42:40 -0300

    Added CRUD feature in /Movies path. Populating DB with initializing data

--- Program.cs
+++ Program.cs
@@ -1,9 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Data;
+using MvcMovie.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDbContext<MvcMovieContext>(
+        options =>
+            options.UseSqlite(
+                builder.Configuration.GetConnectionString("MvcMovieContext")
+                    ?? throw new InvalidOperationException(
+                        "Connection string 'MvcMovieContext' not found."
+                    )
+            )
+    );
+}
+else
+{
+    builder.Services.AddDbContext<MvcMovieContext>(
+        options =>
+            options.UseSqlServer(
+                builder.Configuration.GetConnectionString("ProductionMvcMovieContext")
+                    ?? throw new InvalidOperationException(
+                        "Connection string 'ProductionMvcMovieContext' not found."
+                    )
+            )
+    );
+}
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
